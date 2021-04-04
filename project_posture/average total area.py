e6c79ee97fa322bb4f52d5f2bf776553506e9508
Cheng Zheng <cheng.zheng@pepperdine.edu>
2021-04-05 00:46:02 +0800

    update code
    add relative path to the average total area.py
    
    Co-Authored-By: suii-bit <55775298+suii-bit@users.noreply.github.com>

--- project_posture/average total area.py
+++ project_posture/average total area.py
@@ -1,6 +1,16 @@
+# author: Sunny Qu
+# modified by: Cheng Zheng (add path, so the address don't need to change for different users.
+
+import os
 import pandas as pd
 import numpy as np
-experiment = pd.read_excel('/Users/sunnyqu/Desktop/Project/BDSinfo_modified.xlsx')#读取xlsx中第一个sheet
+from openpyxl import load_workbook
+
+path = os.getcwd()#get current path, i.e. Users/username/.../GitHub/project-posture/project_posture
+path_up1Dir = os.path.dirname(path)#go up one directory, i.e. Users/username/.../GitHub/project-posture
+
+experiment = pd.read_excel(path_up1Dir + '/BDS/BDSinfo_modified.xlsx')#读取xlsx中第一个sheet
+
 column = ['Total Area (cm²)', 'MVPA_minutes.week', 'Subject', 'Vision', 'Surface']
 column_index = [list(experiment.columns).index(i) for i in column]
 exp_ = np.array(experiment.iloc[:,column_index]).tolist()
@@ -14,13 +24,11 @@ for i in exp_:
     else:
         ave_dict[tuple(i[1:])] = i[0]
         ave_num[tuple(i[1:])] = 1
-        
-        
-from openpyxl import load_workbook
-wb = load_workbook('/Users/sunnyqu/Desktop/Project/output.xlsx')
+
+wb = load_workbook(path_up1Dir + '/BDS/aveTotalArea_MVPA.xlsx')
 ws = wb.create_sheet(index = 0, title = 'Average Total Area')
 
-ws['A1'] = 'Average Total area'
+ws['A1'] = 'aveTotalArea'
 ws['B1'] = column[1]
 ws['C1'] = column[2]
 ws['D1'] = column[3]
@@ -29,4 +37,4 @@ ws['E1'] = column[4]
 for i in ave_dict.keys():
     outline = [ave_dict[i]/ave_num[i]] + list(i)
     ws.append(outline)
-wb.save('/Users/sunnyqu/Desktop/Project/output.xlsx')
+wb.save(path_up1Dir + '/BDS/aveTotalArea_MVPA.xlsx')
