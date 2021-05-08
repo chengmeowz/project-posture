2beb0b4ae1a758020305b733c8bf238ca413dab6
suii-bit <55775298+suii-bit@users.noreply.github.com>
2021-05-08 23:12:53 +0800

    Update preprocessing_minmax.py

--- project_posture/preprocessing_minmax.py
+++ project_posture/preprocessing_minmax.py
@@ -6,11 +6,7 @@
 import os
 import numpy as np # used for handling numbers
 import pandas as pd # used for handling the dataset
-from openpyxl import load_workbook
 from sklearn import preprocessing
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder # used for encoding categorical data
-from sklearn.model_selection import train_test_split # used for splitting training and testing data
-from sklearn.preprocessing import StandardScaler # used for feature scaling
 
 path = os.getcwd()#get current path
 path_up1Dir = os.path.dirname(path)#go up one directory
